a735ff580baecbc00c58c5a5c2fd1fc4bb3fe579
WebDesignC <devfproyecto@gmail.com>
2025-08-16 09:14:18 -0700

    Se agrega la ruta categoria.

---
+++ src/routes/CategoriaRoute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const route = express.Router()
+// const categoria =require('../models/Categoria')
+
+route.get('/categoria',()=>{
+
+})
+route.post('/categoria/:id',()=>{
+
+})
+route.put('/categoria/:id',()=>{
+
+})
+route.delete('/categoria/:id',()=>{
+
+})
