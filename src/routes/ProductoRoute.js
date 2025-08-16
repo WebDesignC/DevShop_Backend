52c6e2421a60feb5f3f52fbbafecb9889d3f339f
WebDesignC <devfproyecto@gmail.com>
2025-08-16 09:07:41 -0700

    Creación de rutas basicas

---
+++ src/routes/ProductoRoute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const route = express.Router()
+//const producto =require('../models/Producto')
+
+route.get('/productos',()=>{
+
+})
+route.post('/productos/:id',()=>{
+
+})
+route.put('/productos/:id',()=>{
+
+})
+route.delete('/productos/:id',()=>{
+
+})
---
+++ src/routes/UsuarioRoute.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const route = express.Router()
+// const usuario =require('../models/Usuario')
+
+route.get('/usuario',()=>{
+
+})
+route.post('/usuario/:id',()=>{
+
+})
+route.put('/usuario/:id',()=>{
+
+})
+route.delete('/usuario/:id',()=>{
+
+})
