9e0a1cbb31b31dc70c77043afe305208be3d8bc9
ALVARO CALLEROS SANCHEZ <2024030359@upsin.edu.mx>
2025-09-03 10:16:35 -0700

    feat/user-m-s: Las rutas put y actualzar contra ya funcionan

--- src/routes/UsuarioRoute.js
+++ src/routes/UsuarioRoute.js
@@ -7,9 +7,9 @@ const usuarioSchema = require('../validators/usuarioValidator');
 route.get('/', usuarioController.list);
 route.get('/:id', usuarioController.getById);
 route.post('/', validate(usuarioSchema), usuarioController.create);
-// route.put('/:id', validate(usuarioSchema), usuarioController.update);
+route.put('/:id', validate(usuarioSchema), usuarioController.update);
 route.delete('/:id', usuarioController.remove);
 
-// route.post('/:id/cambiar-password', usuarioController.cambiarPassword);
+route.post('/:id/cambiar-password', usuarioController.cambiarPassword);
 
 module.exports = route;
